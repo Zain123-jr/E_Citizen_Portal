cea8c1af3811d657d6df98046b56bc953d20420c
Muhammad Zain <muhammadzain2000000@gmail.com>
2023-03-27 22:12:58 +0500

    commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from 'react-native';
+
+const App = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center' }} >
+      <Text style={{ fontSize: 40, textAlign: 'center', color: 'black' }} >This is App.js</Text>
+    </View>
+  )
+}
+
+export default App;
