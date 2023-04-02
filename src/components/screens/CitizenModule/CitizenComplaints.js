ce6436f36230ae5c3911e39807c3589fab2f97af
Muhammad Zain <zabideen639@gmail.com>
2023-04-02 16:01:28 +0500

    commit

--- src/components/screens/CitizenModule/CitizenComplaints.js
+++ src/components/screens/CitizenModule/CitizenComplaints.js
@@ -136,7 +136,7 @@ const styles = StyleSheet.create({
     backgroundColor: COLORS.primary,
     padding: 15,
     borderTopLeftRadius:50,
-    borderBottomRightRadius:50,
+    borderBottomRightRadius:50,    
   },
 
   heading: {
